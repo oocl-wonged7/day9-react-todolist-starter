d65f7ed3cbf3c63ab6492db7fce1977468d6a1c5
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-29 18:02:11 +0800

    feat: add pagination

--- src/components/TodoGroup.jsx
+++ src/components/TodoGroup.jsx
@@ -2,15 +2,21 @@ import { useContext } from "react";
 import { TodoContext } from "../App";
 import TodoItem from "./TodoItem";
 
-const TodoGroup = () => {
+const TodoGroup = (props) => {
   const { state } = useContext(TodoContext);
+  const { currentPage, pageSize } = props;
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  const currentTodos = state.slice(startIndex, endIndex);
   return (
     <>
       {state === null || state.length === 0 ? (
         <div>Add the things you need to do today...</div>
       ) : (
         <div>
-          {state.map((todoItem, index) => {
+          {currentTodos.map((todoItem, index) => {
             return <TodoItem todoItem={todoItem} key={todoItem.id + index} />;
           })}
         </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,15 +4,27 @@ import { TodoContext } from "../App";
 import { ACTION } from "../context/todoReducer";
 import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
+import { Pagination } from "antd";
 
 const TodoList = () => {
+  const { state } = useContext(TodoContext);
   const { dispatch } = useContext(TodoContext);
   const [loading, setLoading] = useState(true);
+  const [pageSize,setPageSize] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const HandlePageChange = (current, pageSize) => {
+    setCurrentPage(current);
+    setPageSize(pageSize);
+  };
 
   useEffect(() => {
     getTodoItems()
       .then((todoItems) => {
         dispatch({ type: ACTION.INIT_TODO, payload: todoItems });
+
       })
       .finally(() => {
         setLoading(false);
@@ -20,12 +32,27 @@ const TodoList = () => {
   }, []);
 
   return loading ? (
-    <div>Loading...</div>
+    <Spin
+      indicator={
+        <LoadingOutlined
+          style={{
+            fontSize: 48,
+          }}
+          spin
+        />
+      }
+    />
   ) : (
     <div>
       This is the TodoList Component.
-      <TodoGroup />
+      <TodoGroup currentPage={currentPage} pageSize={pageSize}/>
       <TodoGenerator />
+      <Pagination
+        showSizeChanger
+        onChange={HandlePageChange}
+        defaultCurrent={1}
+        total={state.length}
+      />
     </div>
   );
 };
